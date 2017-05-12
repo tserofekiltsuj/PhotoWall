5a5aaf867b689eb218525519e1ec787900f18cce
student <student@studentde-iMac-237.local>
2017-05-12 17:44:43 +0800

    Add MaskView&LoginView

---
+++ PhotoWall/View/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  PhotoWall
+//
+//  Created by student on 2017/5/12.
+//  Copyright © 2017年 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
